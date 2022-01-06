3045efb64b181bebf0dedcfe5a3df79481917ffe
Wojciech Wroblewski <wroblewskiwojciech161@gmail.com>
2022-01-07 00:33:24 +0100

    added translations and vuetify material components

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/archiprojekt/'
-      : '/'
-  }
+  publicPath: process.env.NODE_ENV === "production" ? "/archiprojekt/" : "/",
+
+  pluginOptions: {
+    i18n: {
+      locale: "pl",
+      fallbackLocale: "pl",
+      localeDir: "locales",
+      enableInSFC: false,
+      enableBridge: false,
+    },
+  },
+
+  transpileDependencies: ["vuetify"],
+};
