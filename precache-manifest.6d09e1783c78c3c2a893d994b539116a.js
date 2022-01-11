ab20d247189c4cbab0ae159e199255b697fefd5b
Wojciech Wroblewski <wroblewskiwojciech161@gmail.com>
2022-01-11 18:36:23 +0100

    'deploy'

---
+++ precache-manifest.6d09e1783c78c3c2a893d994b539116a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca187182107f4a39d747",
+    "url": "/archiprojekt/css/app.2e15b88b.css"
+  },
+  {
+    "revision": "3522a420f1d7b6981b1e",
+    "url": "/archiprojekt/css/chunk-vendors.9541232a.css"
+  },
+  {
+    "revision": "667f1dd1cbd56645ab207fbeee11f2f0",
+    "url": "/archiprojekt/img/logo.667f1dd1.png"
+  },
+  {
+    "revision": "a7d54d9aa36564c66a0ce0716559a39f",
+    "url": "/archiprojekt/img/me.a7d54d9a.jpg"
+  },
+  {
+    "revision": "08ed23de01125990540a6c18b75dd1d6",
+    "url": "/archiprojekt/index.html"
+  },
+  {
+    "revision": "ca187182107f4a39d747",
+    "url": "/archiprojekt/js/app.a5d4e6dd.js"
+  },
+  {
+    "revision": "3522a420f1d7b6981b1e",
+    "url": "/archiprojekt/js/chunk-vendors.d79fc96a.js"
+  },
+  {
+    "revision": "104a74fb033dd1c1d328601f30e5c8a8",
+    "url": "/archiprojekt/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/archiprojekt/robots.txt"
+  }
+]);
